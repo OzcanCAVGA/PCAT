c75829e6d944a38703bf9502b8de4530cbdbe2a7
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2023-12-27 19:20:21 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function () {
-    console.log("Function working")
-}
+const express = require('express');
+const app = express();
+const path = require("path")
+app.use(express.static('public'))
+
+//MIDDLEWARE
+// const myLogger = (req,res,next) =>{
+//     console.log("Log kaydi tutuldu")
+//     next();
+// }
+// const myLogger2 = (req,res,next) =>{
+//     console.log("Log kaydi tutuldu 2")
+//     next();
+// }
+
+// app.use(myLogger)
+// app.use(myLogger2)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucumuz ${port} portunda baslatildi.`)
+})
