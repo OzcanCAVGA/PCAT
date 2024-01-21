1cbd0e7b22f8704785cce431b5a07505fcfd7ce4
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-01-21 15:14:08 +0300

    Add dependenciesç This commit adds the "mongoose" dependency to the package.json file and updates the CSS styling in the templatemo-video-catalog.css file.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose')
 const path = require("path")
 const ejs = require('ejs');
-app.use(express.static('public'))
+const Photo = require('./models/Photo');
+
+const app = express();
+
+//connect DB
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+
+}
+
+
 
 //TEMPLATE ENGINE
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 
 
@@ -14,29 +25,30 @@ app.set("view engine","ejs")
 
 
 //MIDDLEWARE
-// const myLogger = (req,res,next) =>{
-//     console.log("Log kaydi tutuldu")
-//     next();
-// }
-// const myLogger2 = (req,res,next) =>{
-//     console.log("Log kaydi tutuldu 2")
-//     next();
-// }
-
-// app.use(myLogger)
-// app.use(myLogger2)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 //ROUTES
-app.get("/",(req,res)=>{
-    res.render('index')
+app.get("/",async (req, res) => {
+    const photos = await Photo.find({})
+    res.render('index',{
+        photos:photos
+    })
 })
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.render('about')
 })
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
     res.render('add')
 })
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/')
+})
+
 
 
 const port = 3000;
