2d5d2e9cc6e2171f264030d3c1c6cef8db0df2f9
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-01-26 23:05:34 +0300

    Add file upload functionality and method override
    This commit adds the necessary dependencies and middleware for file upload functionality using the express-fileupload package. It also includes the method-override package for HTTP method overriding.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override')
 const path = require("path")
+const fs = require('fs')
 const ejs = require('ejs');
 const Photo = require('./models/Photo');
 
@@ -22,19 +25,21 @@ app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })) // HTML form bicimindeki verileri islemek icin kullaniyoruz.
 app.use(express.json()) // JSON verilerini islemek icin kullaniyoruz.
+app.use(fileUpload())
+app.use(methodOverride('_method'));
 
 
 //ROUTES
 app.get("/", async (req, res) => {
-    const photos = await Photo.find({})
+    const photos = await Photo.find({}).sort('-dateCreated')
     res.render('index', {
         photos: photos
     })
 })
 
-app.get("/photos/:id",async (req, res) => {
+app.get("/photos/:id", async (req, res) => {
     const photo = await Photo.findById(req.params.id)
-    res.render('photo',{ // yonlendirmek istedigim template dosyam
+    res.render('photo', { // yonlendirmek istedigim template dosyam
         photo // template dosyama yollanacak veritabanindaki dosyam
     })
 })
@@ -47,11 +52,41 @@ app.get('/add', (req, res) => {
 })
 
 app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/') // istekten sonra cevap olarak bu sekilde yazilmali. Yoksa sadece istek olarak kalir.
+
+    const uploadDir = 'public/uploads'
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+
+    let uploadedImage = req.files.image
+    let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name
+
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadedImage.name
+        })
+        res.redirect('/')
+    })
 })
 
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    res.render('edit', {
+        photo
+    })
+})
 
+app.put('/photos/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save()
+
+    res.redirect(`/photos/${req.params.id}`)
+})
 
 const port = 3000;
 app.listen(port, () => {
